4f718d983e39d81f61ede8f35c55a834ccd7807f
Chuck <chuckleong21@gmail.com>
2019-09-20 22:25:13 +0800

    with @Amit_Levinson

--- Week 38/national_parks.R
+++
@@ -1,3 +0,0 @@
-df_park_visits <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-09-17/national_parks.csv")
-df_state_pop <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-09-17/state_pop.csv")
-df_gas_price <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-09-17/gas_price.csv")
