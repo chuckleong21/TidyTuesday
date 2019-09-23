b0f004a4dbef684d2d682e018ceb1795cdbfc2a5
Chuck <chuckleong21@gmail.com>
2019-09-23 23:54:28 +0800

    Initial commit

--- Week 39/school_diversity.R
+++ Week 39/school_diversity.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 
-# speed up data loading through local
+# df_school_diversity <- read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-09-24/school_diversity.csv")
+# speed up data loading through local file
 read_csv(here::here("Week 39", "school_diversity.csv"))
 
