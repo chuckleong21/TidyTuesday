9a52d7b26f47b70aa79f6aa74d6e248a768020b8
Chuck <chuckleong21@gmail.com>
2019-09-28 11:28:52 +0800

    final Week 39

--- Week 39/school_diversity.R
+++ Week 39/school_diversity.R
@@ -14,19 +14,8 @@ df_school_diversity <-
         filter(st != "DC") %>% 
         count(leaid, st, school_year, diverse, name = "freq")
 
-# should implicit missing values be filled?
-# df_school_nebraska <- 
-#         df_school_diversity %>% 
-#         filter(st == "NE") %>% 
-#         complete(leaid, school_year, fill = list(st = "NE", freq = 1))
-
-# df_school_diversity %<>% 
-#         filter(st != "NE") %>% 
-#         bind_rows(df_school_nebraska)
-
-
 sub <- str_wrap("Below is the visualization for changes in racial diversity 
-                in schools across states between school years during 1994 and 2016. 
+                in schools across states between school years during 1994 and 2016*. 
                 Although more schools become more racial-diverse, white students remain
                 the majority race in schools.", 80)
 
@@ -40,15 +29,16 @@ df_school_diversity %>%
         geom_flow(alpha = .7) + 
         geom_stratum() + 
         scale_x_discrete(expand = c(0, 0)) +
-        scale_fill_manual(name = "", 
-                          limits = c("Extremely undiverse", "Undiverse", "Diverse"), 
-                          values = c("firebrick", "darkorange", "forestgreen")) +
+        scale_fill_viridis_d(name = "", 
+                          limits = c("Extremely undiverse", "Undiverse", "Diverse"),
+                          option = "C"
+                          ) +
         facet_wrap(~st, scales = "free_y", nrow = 10) +
         theme_minimal(base_family = "Nimrod Cyr-Bold") + 
         labs(x = "", y = "", 
              title = str_to_title("baby steps: more racial-diverse schools"), 
              subtitle = sub, 
-             caption = "Data: NCES | Graphic: @chucc900") + 
+             caption = "Data: NCES | Graphic: @chucc900\n*Data for a certain states are not available") + 
         theme(plot.margin = margin(20, 20, 20, 10), 
               plot.title = element_text(size = 28), 
               plot.subtitle = element_text(size = 20),
@@ -61,4 +51,10 @@ df_school_diversity %>%
               axis.text.y = element_blank(),
               strip.text = element_text(size = 14))
 
-ggsave(here::here("Week 39", "diversity.png"), height = 29.9, width = 42.9, units = "cm")
+if(file.exists(here::here("Week 39", "diversity.png"))) {
+        ask <- askYesNo("'diversity.png' already exists, do you want to replace it? \n")
+        if(ask) ggsave(here::here("Week 39", "diversity.png"), 
+                       height = 29.9, width = 42.9, units = "cm")      
+} else {
+        ggsave(here::here("Week 39", "diversity.png"), height = 29.9, width = 42.9, units = "cm")
+}
