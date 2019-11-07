b692b58eca705f83d3bb9759206ac5f1cf8b6da6
Chuck Leong <chuckleong21@gmail.com>
2019-11-07 17:27:46 +0800

    added `save` for storing a local copy of csv

--- tidy_tuesday.R
+++ tidy_tuesday.R
@@ -3,7 +3,7 @@
 #' @details getting data from #TidyTuesday repo with ease! No more 
 #' excessive url slug passed to \code{read_csv()}. It opens a browser
 #' being directed to most recent week of tidytuesday's data folder when
-#' only \code{NULL} are passed to arguments. It recognizes week number
+#' only \code{NULL} are passed to arguments. It recognizes iso week number
 #' so you don't pass date explicitly. Only week 22 to 51 are available
 #' for year 2018, as for previous weeks they are all on Mondays.
 #' 
@@ -11,10 +11,12 @@
 #' @param filename Single character for csv file
 #' @param year numeric. 2018 or 2019
 #' @param week numeric. Between 1 and 52
+#' @param save boolean. Do you want to save a local copy?
 #' 
 #' @rdname TidyTuesday
 #' 
-tidy_tuesday <- function(filename = NULL, year = NULL, week = NULL) {
+tidy_tuesday <- function(filename = NULL, year = NULL, week = NULL, 
+                         save = FALSE) {
         `%||%` <- function(lhs, rhs) if(!is.null(lhs)) lhs else rhs
         
         today <- Sys.Date()
@@ -47,5 +49,28 @@ tidy_tuesday <- function(filename = NULL, year = NULL, week = NULL) {
                 stop("`filename` is a character of length 1.")
         } else if(!grepl("\\.csv", filename)) {
                 stop("Incorrect file type.")
-        } else readr::read_csv(url)
+        } else io <- readr::read_csv(url)
+        
+        if(save) {
+                thisweek <- paste("[W|w]eek[.|\\s|-]?", week)
+                directories <- list.files()
+                folder_exists <- grepl(thisweek, directories)
+                if(any(folder_exists)) {
+                        path <- here::here(directories[which(folder_exists)], 
+                                           filename)
+                } else {
+                        thisweek <- paste("Week", week)
+                        dir.create(here::here(thisweek))
+                        path <- here::here(thisweek, filename)
+                }
+                
+                # skip writing files if it already existed
+                isduplicate <- list.files(here::here(thisweek))
+                isduplicate <- grepl(filename, isduplicate)
+                isduplicate <- any(isduplicate)
+                if(!isduplicate) readr::write_csv(x = io, path = path)
+                return(io)
+        }
+        
+        if(exists("io")) return(io)
 }
