993da416b2964164306a19bc4da3f7b0a2563858
Chuck Leong <chuckleong21@gmail.com>
2019-11-13 01:30:57 +0800

    tidy up

--- tidy_tuesday.R
+++ tidy_tuesday.R
@@ -1,16 +1,16 @@
 #' @title Browse #TidyTuesday Repo And Load csv files 
 #' 
-#' @details getting data from #TidyTuesday repo with ease! No more 
-#' excessive url slug passed to \code{read_csv()}. It opens a browser
+#' @details Getting data from #TidyTuesday repo with ease! No more 
+#' excessively long url slug passed to \code{read_csv()}. It opens a browser
 #' being directed to most recent week of tidytuesday's data folder when
 #' only \code{NULL} are passed to arguments. It recognizes iso week number
 #' so you don't pass date explicitly. Only week 22 to 51 are available
-#' for year 2018, as for previous weeks they are all on Mondays.
+#' for year 2018, as for previous weeks are all on Mondays.
 #' 
 #' 
 #' @param filename Single character for csv file
-#' @param year numeric. 2018 or 2019
-#' @param week numeric. Between 1 and 52
+#' @param year integer 2018 or 2019
+#' @param week integer Between 1 and 52
 #' @param save boolean. Do you want to save a local copy?
 #' 
 #' @rdname TidyTuesday
@@ -20,11 +20,11 @@ tidy_tuesday <- function(filename = NULL, year = NULL, week = NULL,
         `%||%` <- function(lhs, rhs) if(!is.null(lhs)) lhs else rhs
         
         today <- Sys.Date()
-        year <- year %||% format(today, "%Y")
+        year <- as.integer(year %||% format(today, "%Y"))
         date_origin <- as.Date(paste0(year, "-01-01"))
         weekday <- lubridate::wday(date_origin + 0:6)
         first_tue <- (date_origin + 0:6)[which(weekday == 3)]
-        week <- week %||% lubridate::week(today)
+        week <- week %||% as.integer(lubridate::week(today))
         if(!week > 0 | !week < 53) {
                 stop("Please specify a week number between 1 and 52.")
         }
@@ -69,8 +69,6 @@ tidy_tuesday <- function(filename = NULL, year = NULL, week = NULL,
                 isduplicate <- grepl(filename, isduplicate)
                 isduplicate <- any(isduplicate)
                 if(!isduplicate) readr::write_csv(x = io, path = path)
-                return(io)
-        }
-        
+                }
         if(exists("io")) return(io)
 }
